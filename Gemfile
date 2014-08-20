a4a19b5c33dc973c19cc58c7f5638d150372fccf
Benny C. Wong <bwong337@gmail.com>
2014-08-19 20:25:16 -0700

    some style

--- Gemfile
+++ Gemfile
@@ -9,6 +9,10 @@ gem 'pg'
 
 gem 'activeadmin'
 
+#Bootstrap
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
+
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,25 +13,25 @@ class User < ActiveRecord::Base
     REFERRAL_STEPS = [
         {
             'count' => 5,
-            "html" => "Shave<br>Cream",
+            "html" => "Hand<br>Wraps",
             "class" => "two",
             "image" =>  ActionController::Base.helpers.asset_path("refer/cream-tooltip@2x.png")
         },
         {
             'count' => 10,
-            "html" => "Truman Handle<br>w/ Blade",
+            "html" => "Water<br>Bottle",
             "class" => "three",
             "image" => ActionController::Base.helpers.asset_path("refer/truman@2x.png")
         },
         {
             'count' => 25,
-            "html" => "Winston<br>Shave Set",
+            "html" => "Fight Fit<br>Beginner Package",
             "class" => "four",
             "image" => ActionController::Base.helpers.asset_path("refer/winston@2x.png")
         },
         {
             'count' => 50,
-            "html" => "One Year<br>Free Blades",
+            "html" => "One Month<br>of Membership",
             "class" => "five",
             "image" => ActionController::Base.helpers.asset_path("refer/blade-explain@2x.png")
         }
