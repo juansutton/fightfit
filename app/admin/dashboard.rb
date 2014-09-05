e3daee1815b792632d57353faebec3050f3a833e
Benny C. Wong <bwong337@gmail.com>
2014-09-05 01:37:17 -0400

    adding referral on dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,69 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+ 
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    users_with_referrals = User.all
+    no_prize_users = []
+    prize_1_users = []
+    prize_2_users = []
+    prize_3_users = []
+    prize_4_users = []
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+    users_with_referrals.each do |user|
+      if user.referrals.count.between?(1,4)
+        no_prize_users << user
+      elsif user.referrals.count.between?(5,9)
+        prize_1_users << user
+
+      elsif user.referrals.count.between?(10,24)
+        prize_2_users << user
+
+      elsif user.referrals.count.between?(25,49)
+        prize_3_users << user
+
+      elsif user.referrals.count >= 50
+        prize_4_users << user
+
+      end
+
+
+
+
+
+    end
+
+    columns do
+      show_referral_table("Users with at 1-4 referral", no_prize_users)
+      show_referral_table("Users with at 5-9 referral", prize_1_users)
+      show_referral_table("Users with at 10-24 referral", prize_2_users)
+      show_referral_table("Users with at 25-49 referral", prize_3_users)
+      show_referral_table("Users with great than 50 referral", prize_4_users)
+    end
+
   end # content
+end
+
+
+
+
+def show_referral_table (name, users)
+  column do
+    section "#{users.count} #{name}"  do  
+      table_for users do
+        show_prize
+      end
+    end
+  end
+end
+
+def show_prize
+  column :email do |user|  
+      link_to user.email, admin_user_path(user)  
+  end  
+  column "Referral Count" do |user|
+    user.referrals.count  
+  end
 end
